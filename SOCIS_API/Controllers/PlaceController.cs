da645181b6e4420ae826160be270f50e77ca9f96
dysha2 <dyha035@gmail.com>
2023-05-04 16:39:21 +0500

    push

---
+++ SOCIS_API/Controllers/PlaceController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOCIS_API.Controllers
+{
+    [Route("crud/[controller]")]
+    public class PlaceController : Controller
+    {
+        ICrudRep crudRep;
+
+        public PlaceController(ICrudRep crudRep)
+        {
+            this.crudRep = crudRep;
+        }
+        [HttpGet("GetAll"),Authorize]
+        public ActionResult<IEnumerable<Place>> GetAll()
+        {
+            return crudRep.Read<Place>();
+        }
+    }
+}
